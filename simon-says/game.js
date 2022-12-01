0afb9403e2241abfe80e94a33f524f6b44ec2585
Yoshi <yoshimejia00@gmail.com>
2022-12-01 11:41:16 -0500

    adding button fade & audio

--- simon-says/game.js
+++ simon-says/game.js
@@ -5,4 +5,7 @@ function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    audio.play();
 }
