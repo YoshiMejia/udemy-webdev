d7d12d1556aae544f994886af12906275e82e161
Yoshi <yoshimejia00@gmail.com>
2022-12-01 12:03:20 -0500

    reordered functions + DRY'd sound

--- simon-says/game.js
+++ simon-says/game.js
@@ -1,11 +1,23 @@
 let buttonColours = ['red', 'blue', 'green', 'yellow'];
 let gamePattern = [];
+let userClickedPattern = [];
+
+$(".btn").click(function(e){
+    let userChosenColour = e.target.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    //  console.log(userClickedPattern);
+});
 
 function nextSequence(){
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    $("#" + randomChosenColour).fadeIn(200).fadeOut(200).fadeIn(200);
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    let audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
